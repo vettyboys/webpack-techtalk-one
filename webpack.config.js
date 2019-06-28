6345cb7424a10a504f1ae81defd88720ad76708c
raj <boyswithoutwork@gmail.com>
2019-06-27 20:49:39 -0500

    Added HTMLWebpackPlugin, HTMLloader, Fileloader, WebpackCleanPlugin, WebpackDevServer. Moved assets folder under src folder.
      - package.json (u)
      - index.html (u)
      - webpack.config.js (u)
      - src/assets/webpack.png (moved)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const path =require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
@@ -10,8 +12,29 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use:["style-loader", "css-loader"],
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.png$/,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "assets"
+                    }
+                }]
             }
         ]
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: "./src/index.html"
+        }),
+    ]
 }
